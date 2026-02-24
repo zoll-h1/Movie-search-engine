6d029e799a253de3e0668b691a5e1c2c46d63171
zoll-h1 <nurbekerkibaev9@gmail.com>
2026-02-24 17:14:34 +0600

    Moviecard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css"
+import MovieCard from "./assets/Components/Movie";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello my Nigga</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-         Hell nah broh
-      </p>
+    <MovieCard movie={{title: "The Janbolot" , release_date: "2026"}}/>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
---
+++ frontend/src/assets/Components/Movie.jsx
@@ -0,0 +1,23 @@
+function MovieCard({movie}) {
+
+    function onFavoriteClick() {
+        alert(`Added ${movie.title} to favorites!`);
+    }
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.poster} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>
+                        
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    );
+}
+export default MovieCard;
