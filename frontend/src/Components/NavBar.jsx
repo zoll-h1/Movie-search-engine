784edfdac695ddd64e58233f08511f38380fe15d
zoll-h1 <nurbekerkibaev9@gmail.com>
2026-03-03 06:38:12 +0600

    done

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-           <Link to="/">ZOlL_H! MOVIES</Link> 
+           <Link to="/">Movies App</Link> 
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
